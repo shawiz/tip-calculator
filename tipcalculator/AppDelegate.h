cef19a537cdae13c09741c48a3db0186a9094904
Sean Ren <shawiz@gmail.com>
2014-02-27 09:51:42 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sean Ren on 2/27/14.
+//  Copyright (c) 2014 Sean Ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
